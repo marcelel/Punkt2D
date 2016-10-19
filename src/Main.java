8fd70b561cada7dfe25b0155256b3362e62aaf1d
marcelel <marcelel321@gmail.com>
2016-10-19 09:27:22 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -15,11 +16,31 @@ public class Main {
             int decision = in.nextInt();
             switch (decision){
                 case 1:
-                    points.add(Punkt3D.addPoint());
+                    System.out.println("Podaj wspolrzedne");
+                    try {points.add(new Punkt3D(in.nextDouble(), in.nextDouble(), in. nextDouble()));}
+                    catch(InputMismatchException e){
+                        System.out.println("Zły format");
+                   }
                     break;
                 case 2:
                     for(Punkt3D p : points){
                         System.out.println("X: " + p.getX() + "\nY: " + p.getY() + "\nZ: " + p.getZ() + "\n");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Podaj dwa punkty od 0 do " + points.size());
+                    try{
+                        int point1 = in.nextInt();
+                        int point2 = in.nextInt();
+                        if (point1 < 0 || point1 >= points.size() || point2 < 0 || point2 >= points.size()){
+                            System.out.println("Złe punkty");
+                        }
+                        else{
+                            System.out.println(points.get(point1).distance(points.get(point2)));
+                        }
+                    }
+                    catch(InputMismatchException e) {
+                        System.out.println("Zły format");
                     }
                     break;
                 case 4:
--- src/Punkt3D.java
+++ src/Punkt3D.java
@@ -20,14 +20,4 @@ public class Punkt3D extends Punkt2D{
     public double distance(Punkt3D obj){
         return Math.sqrt(Math.pow((obj.x - x), 2) + Math.pow((obj.y - y), 2) + Math.pow((obj.z - z), 2));
     }
-    public static Punkt3D addPoint(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Podaj x");
-        double x = in.nextDouble();
-        System.out.println("Podaj y");
-        double y = in.nextDouble();
-        System.out.println("Podaj z");
-        double z = in.nextDouble();
-        return new Punkt3D(x, y, z);
-    }
 }
